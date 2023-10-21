f222fd8e218291318108250757d27bd182af0e5f
Talgat_Pardabekov <talgat_pardabekov@epam.com>
2023-10-21 15:21:37 +0600

    added eureka

--- Resource_Project/src/main/java/com/epam/feign/SongService.java
+++ Resource_Project/src/main/java/com/epam/feign/SongService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "song", url = "${song-service.url}")
+@FeignClient(name = "SONG-SERVICE"/*, value = "song", url = "${song-service.url}"*/)
 //TODO: need uri for song service
 public interface SongService {
 
